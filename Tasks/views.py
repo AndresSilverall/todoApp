8e07452ef807a5ee4f24944901e2efe43735df26
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-16 21:09:48 -0500

    modelo de datos de la App configurado

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Task
 
 # Create your views here.
 def index(request):
-    return render(request, "tasks/list.html")
+    task = Task.objects.all()
+    context = {
+        "tasks": task
+    }
+    return render(request, "tasks/list.html", context=context)
 
