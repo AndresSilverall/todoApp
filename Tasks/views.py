544a9e80976f617d99939ad422142c58bd50f729
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-16 21:42:42 -0500

    formulario de datos configurado

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . models import Task
+from. forms import TaskForm
 
 # Create your views here.
 def index(request):
     task = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("index")
     context = {
-        "tasks": task
+        "tasks": task,
+        "form": form
     }
     return render(request, "tasks/list.html", context=context)
 
