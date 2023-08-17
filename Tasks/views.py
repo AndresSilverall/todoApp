f0f11ea0732f04e7113b591ba46284429f6290c7
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-08-17 16:00:51 -0500

    se agrego la interfaz grafica a la App

--- Tasks/views.py
+++ Tasks/views.py
@@ -12,7 +12,7 @@ def index(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
+        return redirect("index")
     context = {
         "tasks": task,
         "form": form
